6fa6f03f2f2a2b12d5b4cda6a05cb85f9d7b7edf
K.Ozaki <ozaki@nyam.co.jp>
2020-11-24 07:09:18 +0000

    Validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_052251) do
+ActiveRecord::Schema.define(version: 2020_11_24_065346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
